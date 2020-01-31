4c683c0d20a41f5f48185df4d6bd75ba7210407d
tomzacchia <tomzacchia@live.com>
2020-01-31 22:58:43 +0700

    Refactored menu-container and menu-item

--- src/pages/hompage/hompage.component.jsx
+++ src/pages/hompage/hompage.component.jsx
@@ -1,42 +1,11 @@
 import React from 'react';
-
 import './homepage.styles.scss';
+import MenuContainer from '../../components/menu-container/menu-container.component';
 
 const HomePage = () => {
   return (
     <div className="homepage">
-      <div className="menu-container">
-        <div className="menu-item">
-          <div className="content-container">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content-container">
-            <h1 className="title">JACKETS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content-container">
-            <h1 className="title">SHOES</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content-container">
-            <h1 className="title">WOMEN&apos;S</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content-container">
-            <h1 className="title">MEN&apos;S</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
+      <MenuContainer />
     </div>
   );
 };
