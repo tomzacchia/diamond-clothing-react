ac7bad254e3f5f769e673129d41db87d274a2d57
tomzacchia <tomzacchia@live.com>
2020-01-31 15:24:47 +0700

    Added basic set up of menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import HomePage from './hompage.component';
+
 function App() {
   // this is a test
-  return <div> HELLO WORLD </div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/hompage.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="menu-container">
+        <div className="menu-item">
+          <div className="content-container">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content-container">
+            <h1 className="title">JACKETS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content-container">
+            <h1 className="title">SHOES</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content-container">
+            <h1 className="title">WOMEN&apos;S</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content-container">
+            <h1 className="title">MEN&apos;S</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
