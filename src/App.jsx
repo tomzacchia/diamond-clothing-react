23453f5cec460a266db5c2b882d12328fbfd3a0d
tomzacchia <tomzacchia@live.com>
2020-02-01 19:30:13 +0700

    Added routing to re-direct to shop/hats
    - disabled 2 jsx-a11y rules for the sake of development:
    - no-static-element-interactions
    - click-events-have-key-events

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/hompage/hompage.component';
 import './App.styles.scss';
 
+const hatsPage = () => (
+  <div>
+    <h1> HATS </h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* Switch only renders the first route that matches path */}
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={hatsPage} />
+      </Switch>
+
+      {/* https://stackoverflow.com/questions/45122800/react-router-switch-behavior */}
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ history, match, remainingMenuItemProps }) => {
+  const { title, imageUrl, size, linkUrl } = remainingMenuItemProps;
   let menuItemClass = 'menu-item';
 
   if (size) {
     menuItemClass = menuItemClass.concat(` ${size}`);
   }
 
+  const onClickHandler = () => {
+    history.push(`${match.url}${linkUrl}`);
+  };
+
   return (
-    <div className={menuItemClass}>
+    <div className={menuItemClass} onClick={onClickHandler}>
       <div
         style={{ backgroundImage: `url(${imageUrl})` }}
         className="background-image"
@@ -22,4 +28,6 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+// withRouter takes a component and modifies it to give
+// access to history, location, match
+export default withRouter(MenuItem);
