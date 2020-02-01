10633400bb616ad65cfebd9b83ffac6cd88b8135
tomzacchia <tomzacchia@live.com>
2020-02-01 23:08:59 +0700

    Add header with SVG and 2 option links
    - option links re-direct to shop, contact etc..

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import './App.styles.scss';
 
 import HomePage from './pages/home-page/home-page.component';
 import ShopPage from './pages/shop-page/shop.component';
-import './App.styles.scss';
+import Header from './components/header/hader.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
