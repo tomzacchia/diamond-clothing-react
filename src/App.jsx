4f7e52216da6123571e7f48bebe3459977779759
tomzacchia <tomzacchia@live.com>
2020-02-04 06:45:05 +0700

    Add auth.onAuthStateChanged to get status of current user, if any

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
 import './App.styles.scss';
+
+import { Route, Switch } from 'react-router-dom';
+import { auth } from './firebase/firebase.utils';
 
 import HomePage from './pages/home-page/home-page.component';
 import ShopPage from './pages/shop-page/shop.component';
 import Header from './components/header/hader.component';
 import Authentication from './pages/authentication/authentication.component';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/authentication" component={Authentication} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  unsubscribeFromAuth = null;
+
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    // calling unsubscribeFromAuth will unsubscribe from updates from auth server
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/authentication" component={Authentication} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
