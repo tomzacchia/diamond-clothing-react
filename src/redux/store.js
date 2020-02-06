deb9d3d5fb9f775297c6c492f0312e25db3d105e
tomzacchia <tomzacchia@live.com>
2020-02-07 00:06:00 +0700

    Add Redux devtools extension to store for debugging

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,6 +6,10 @@ import rootReducer from './root-reducer';
 const middlewares = [logger];
 
 // pass middleware arrays into applyMiddlewares
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(...middlewares) + window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
