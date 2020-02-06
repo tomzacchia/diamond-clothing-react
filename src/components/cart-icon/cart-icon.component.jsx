fc439e3f15ff0eb062669df60ce634e544529612
tomzacchia <tomzacchia@live.com>
2020-02-07 05:18:14 +0700

    Refactor cart-item.component to use selector to compute itemCount

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,20 +6,11 @@ import { connect } from 'react-redux';
 import { ReactComponent as ShopIcon } from '../../assets/shopping-bag.svg';
 import { toggleCartDropdown } from '../../redux/cart/cart.actions';
 
-const CartIcon = ({ toggleCartDropdown, cartItems }) => {
-  let totalItemsQuantity = 0;
-
-  if (cartItems.length > 0) {
-    totalItemsQuantity = cartItems.reduce(
-      (total, cartItem) => total + cartItem.quantity,
-      0
-    );
-  }
-
+const CartIcon = ({ toggleCartDropdown, itemCount }) => {
   return (
     <div className="cart-icon" onClick={toggleCartDropdown}>
       <ShopIcon className="shop-icon" />
-      <span className="item-count">{totalItemsQuantity}</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
@@ -28,8 +19,16 @@ const mapDispatchToProps = dispatch => ({
   toggleCartDropdown: () => dispatch(toggleCartDropdown())
 });
 
+/* 
+  mapStateToProps is called regardless if the property we destructured
+  changed or not, i.e toggleCartDropdown changes but mapStateToProps is executed
+  regardless because the new Store state is a new object when compared to 
+  the old Store state
+*/
 const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+  // example of redux selector, we are manipulating data to get a new
+  // custom property
+  itemCount: cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
