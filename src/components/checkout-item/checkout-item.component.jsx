4e84db2ca14f26009d8bb1b7fd65a98d60767af4
tomzacchia <tomzacchia@live.com>
2020-02-08 14:00:20 -0500

    Add decrementCartItemQuantity and decrementCartItemQuantity actions to checkout-item.component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem }) => {
-  const { name, imageUrl, price, quantity } = cartItem;
+import { connect } from 'react-redux';
+import {
+  deleteCartItemById,
+  decrementCartItemQuantity,
+  addCartItem
+} from '../../redux/cart/cart.actions';
+
+const CheckoutItem = ({
+  cartItem,
+  deleteCartItem,
+  decrementQuantity,
+  incrementQuantity
+}) => {
+  const { name, imageUrl, price, quantity, id } = cartItem;
+
   return (
     <div className="checkout-item">
       <div className="image-container">
@@ -12,16 +25,30 @@ const CheckoutItem = ({ cartItem }) => {
       <span className="name">{name}</span>
 
       <span className="quantity">
-        <div className="arrow">&#10094;</div>
+        <div className="arrow" onClick={() => decrementQuantity(id)}>
+          &#10094;
+        </div>
         <span className="value">{quantity}</span>
-        <div className="arrow">&#10095;</div>
+        <div className="arrow" onClick={() => incrementQuantity(cartItem)}>
+          &#10095;
+        </div>
       </span>
 
       <span className="price">{price}</span>
 
-      <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={() => deleteCartItem(id)}>
+        &#10005;
+      </div>
     </div>
   );
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  deleteCartItem: cartItemId => dispatch(deleteCartItemById(cartItemId)),
+  decrementQuantity: cartItemId => {
+    dispatch(decrementCartItemQuantity(cartItemId));
+  },
+  incrementQuantity: cartItem => dispatch(addCartItem(cartItem))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
