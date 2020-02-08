d8f878e104b0f972e2fd5c745e09c92813928b95
tomzacchia <tomzacchia@live.com>
2020-02-08 17:30:38 -0500

    Refactor shop.component to render CollectionsOverview component. Preparing for nested Routing.

--- src/pages/shop-page/shop.component.jsx
+++ src/pages/shop-page/shop.component.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
 import './shop.styles.scss';
-import collectionsMockData from './shop.data';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-class ShopPage extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      collections: collectionsMockData
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-
-    const collectionsElements = collections.map(collection => {
-      const { id, title, items } = collection;
-      return <CollectionPreview key={id} title={title} previewItems={items} />;
-    });
-
-    return <div className="shop-page">{collectionsElements}</div>;
-  }
-}
+const ShopPage = () => {
+  return (
+    <div className="shop-page">
+      <CollectionsOverview />
+    </div>
+  );
+};
 
 export default ShopPage;
