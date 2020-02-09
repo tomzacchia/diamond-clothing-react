e3d5713c66b0c731d3133f520d0184783eafba74
tomzacchia <tomzacchia@live.com>
2020-02-09 13:09:31 -0500

    Refactor store.js to resolve bug where devtools_extension is not available in build

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+// optional: logger
+const middlewares = [];
 
-export const store = createStore(
-  rootReducer,
-  applyMiddleware(...middlewares) +
-    (window.__REDUX_DEVTOOLS_EXTENSION__ || null) &&
-    (window.__REDUX_DEVTOOLS_EXTENSION__() || null)
-);
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+
+export const store = createStore(rootReducer, enhancer);
 
 // create persisted version of our store
 export const persistor = persistStore(store);
