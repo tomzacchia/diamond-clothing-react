260459adda695530982a2a2eaaf32f004af5c790
tomzacchia <tomzacchia@live.com>
2020-02-13 10:52:22 -0500

    Remove comments

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -2,16 +2,5 @@ import { all, call } from 'redux-saga/effects';
 import { fetchCollectionsStart } from './shop/shop.sagas';
 
 export default function* rootSaga() {
-  /* 
-    if we have N sagas, the 2nd to Nth yield sagaFunction() would
-    have to wait for those lexically called before it to yield or
-    return therefore we use all() to initialize all functions at
-    the start of the application
-  */
   yield all([call(fetchCollectionsStart)]);
-  /*
-    yield fetchCollectionsStart();
-    yield fetchCollectionsStart();
-    yield fetchCollectionsStart();
-  */
 }
