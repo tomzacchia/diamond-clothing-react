ee545ae5245f543c295cb636ff407f833452af32
tomzacchia <tomzacchia@live.com>
2020-02-12 22:44:12 -0500

    Delete deprecated fetchCollectionsStartAsync function

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -18,18 +18,3 @@ export const fetchCollectionsFailure = errorMessage => ({
   type: shopActionTypes.FETCH_COLLECTIONS_FAILURE,
   payload: errorMessage
 });
-
-export const fetchCollectionsStartAsync = () => {
-  return dispatch => {
-    const collectionRef = firestore.collection('collections');
-    dispatch(fetchCollectionsStart());
-
-    collectionRef
-      .get()
-      .then(snapshot => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        dispatch(fetchCollectionSuccess(collectionsMap));
-      })
-      .catch(error => dispatch(fetchCollectionsFailure(error.message)));
-  };
-};
