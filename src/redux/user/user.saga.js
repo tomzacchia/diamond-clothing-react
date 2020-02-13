11e5b6ef6126b66f5e50057de0d5270ab35c80b4
tomzacchia <tomzacchia@live.com>
2020-02-13 14:48:55 -0500

    Refactor signInWithGoogle and signInWithEmail to use getUserSnapshot

--- src/redux/user/user.saga.js
+++ src/redux/user/user.saga.js
@@ -7,21 +7,38 @@ import {
   googleProvider,
   createUserProfileDocument
 } from '../../firebase/firebase.utils';
-import { googleSignInSuccess, googleSignInFail } from './user.actions';
+import { signInSuccess, signInFail } from './user.actions';
+
+export function* getUserSnapshot(user) {
+  try {
+    const userRef = yield call(createUserProfileDocument, user);
+    const userSnapshot = yield userRef.get();
+
+    yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+  } catch (error) {
+    put(signInFail(error.message));
+  }
+}
 
 export function* signInWithGoogle() {
   try {
     const { user } = yield auth.signInWithPopup(googleProvider);
 
-    const userRef = yield call(createUserProfileDocument, user);
-
-    const userSnapshot = yield userRef.get();
-
-    yield put(
-      googleSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
-    );
+    yield getUserSnapshot(user);
   } catch (error) {
-    put(googleSignInFail(error.message));
+    put(signInFail(error.message));
+  }
+}
+
+export function* signInWithEmail({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const { user } = yield auth.signInWithEmailAndPassword(email, password);
+
+    yield getUserSnapshot(user);
+  } catch (error) {
+    put(signInFail(error.message));
   }
 }
 
@@ -29,6 +46,10 @@ export function* googleSignInSaga() {
   yield takeLatest(userActionsTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
 
+export function* signInWithEmailSaga() {
+  yield takeLatest(userActionsTypes.EMAIL_SIGN_IN_START, signInWithEmail);
+}
+
 export function* userSagas() {
-  yield all([call(googleSignInSaga)]);
+  yield all([call(googleSignInSaga), call(signInWithEmailSaga)]);
 }
