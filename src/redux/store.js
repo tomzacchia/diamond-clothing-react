60766f45841826402b2646b4b8c9fc1aa6a6720d
tomzacchia <tomzacchia@live.com>
2020-02-19 13:33:13 -0500

    Add node_env conditional to add logger to redux middleware stack

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,14 +9,17 @@ import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [sagaMiddleware];
+let middlewares = [sagaMiddleware];
 
 const composeEnhancers =
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
     : compose;
 
-// const enhancer = composeEnhancers(applyMiddleware(...middlewares, logger));
+if (process.env.NODE_ENV === 'development') {
+  middlewares = [...middlewares, logger];
+}
+
 const enhancer = composeEnhancers(applyMiddleware(...middlewares));
 
 export const store = createStore(rootReducer, enhancer);
