72436767899ddbfcbd32b7af0afd038d89cd576d
tomzacchia <tomzacchia@live.com>
2020-02-20 18:02:32 -0500

    Refactor CartDropdown to use styled-components

---
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const CartDropdownContainer = styled.div`
+  position: absolute;
+  width: 240px;
+  height: 340px;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  border: 1px solid black;
+  background-color: white;
+  top: 50px;
+  right: 22px;
+  z-index: 5;
+  
+  button {
+    margin-top: auto;
+  }
+
+  ${({ theme }) => theme.md`
+    top: 80px;
+    right: 52px;
+  `}
+`;
+
+export const EmptyMessageContainer = styled.span`
+  font-size: 18px;
+  margin: 50px auto;
+`;
+
+export const CartItemsContainer = styled.div`
+  height: 240px;
+  display: flex;
+  flex-direction: column;
+  overflow: scroll;
+`
